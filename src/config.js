dfc4dc452b6334c95d69da602841e93f22b394bb
oempe3 <oem.pe3@gmail.com>
2026-05-22 20:14:01 -0300

    Update APPS_SCRIPT_URL in config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ window.APP_CONFIG = {
   // Cole aqui a URL /exec do Google Apps Script.
   // Exemplo:
   // APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxxxxxxx/exec'
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw05p20d1cXQmPfLuqBKmsaohOZs4pdhRxnDa-9OaZzNs3DHQKLxS1umjb8tcKpk6_-/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzv49WTBEogaYEDccyWMKkcIsLLRjRo_T6agBNt9qX2aklxUoSKHw1TXMaajMAt5OjX/exec',
 
   // Nome exibido no sistema.
   APP_NAME: 'Controle de Consumíveis',
